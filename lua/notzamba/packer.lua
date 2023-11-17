d6ceac9be7db62f5cdce397fb7d9af1612b32b6f
HenriqueZ18 <Hique.rz03@gmail.com>
2023-11-17 17:58:43 -0300

    changed telescope version

--- lua/notzamba/packer.lua
+++ lua/notzamba/packer.lua
@@ -5,8 +5,9 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use{
-	    'nvim-telescope/telescope.nvim',tag = '0.1.1',
-	    -- or 			     , branch = '0.1.x',
+	    'nvim-telescope/telescope.nvim',
+        -- tag = '0.1.4', 			     
+        branch = '0.1.x',
 	    requires = {{'nvim-lua/plenary.nvim'}}
     }
 
